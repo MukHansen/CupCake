6abdeefab96c220cef4b4950609b5e80ef032407
Bruger <Bruger@10.50.130.0>
2019-03-05 13:04:36 +0100

    Tilføjet toString metode + email i constructeren

--- src/main/java/data/User.java
+++ src/main/java/data/User.java
@@ -13,6 +13,7 @@ public class User {
     private String name;
     private String password;
     private int balance;
+    private String email;
 
     public String getName() {
         return name;
@@ -21,12 +22,17 @@ public class User {
     public int getBalance() {
         return balance;
     }
-    private String email;
 
-    public User(String name, String password, int balance) {
+    public User(String name, String password, String email, int balance) {
         this.name = name;
         this.password = password;
+        this.email = email;
         this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "name=" + name + ", password=" + password + ", balance=" + balance + ", email=" + email + '}';
     }
     
 }
