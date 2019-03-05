7b8691a48324bfb2d34e20224331f62517d4837d
Bruger <Bruger@10.50.130.0>
2019-03-05 13:03:56 +0100

    getUser + createUser metoder oprettet

--- src/main/java/data/DBAccessor.java
+++ src/main/java/data/DBAccessor.java
@@ -6,6 +6,7 @@
 package data;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -16,18 +17,21 @@ import java.util.List;
  * @author Bruger
  */
 public class DBAccessor {
-    
+
+    /*
     public static void main(String[] args) {
+        System.out.println(getUser("John").toString());
+        createUser("John", "1234", "Teeest@testmail.dk", 1000);
+
         System.out.println("Start");
         getAllCupCakes();
         String Jordbær = "Jordbær";
         CompleteCupCake j = getCupCake(Jordbær);
         System.out.println("CupCake found: " + j.getName());
-        System.out.println(getAllCupCakes().toString());
-        
+        System.out.println(getAllCupCakes().toString()); 
     }
-
-   static public List<CompleteCupCake> getAllCupCakes() {
+     */
+    public List<CompleteCupCake> getAllCupCakes() {
         ArrayList<CompleteCupCake> list = new ArrayList<>();
 
         try {
@@ -51,7 +55,7 @@ public class DBAccessor {
         return list;
     }
 
-    static public CompleteCupCake getCupCake(String name) {
+    public CompleteCupCake getCupCake(String name) {
         CompleteCupCake CupCake = null;
 
         try {
@@ -63,7 +67,7 @@ public class DBAccessor {
             Statement stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
-                
+
                 int price = rs.getInt("c_price");
                 CupCake = new CompleteCupCake(name, price);
             }
@@ -74,5 +78,48 @@ public class DBAccessor {
         }
         System.out.println(CupCake + "++++++++++++++++");
         return CupCake;
+    }
+
+    public User getUser(String name) {
+        User user = null;
+
+        try {
+            DBConnector c = new DBConnector();
+
+            String query = "SELECT * FROM Users WHERE UserName = '" + name + "';";
+
+            Connection connection = c.getConnection();
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String password = rs.getString("UserPassword");
+                String email = rs.getString("UserEmail");
+                int balance = rs.getInt("UserBalance");
+                user = new User(name, password, email, balance);
+            }
+            return user;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    public User createUser(String name, String password, String email, int balance) {
+        User user = null;
+
+        try {
+            DBConnector c = new DBConnector();
+            Connection connection = c.getConnection();
+
+            String insert = "INSERT INTO Users VALUES ('" + name + "','" + password + "'," + balance + ",'" + email + "');";
+
+            Statement stmt = connection.createStatement();
+            PreparedStatement preparedStmt = connection.prepareStatement(insert);
+            preparedStmt.execute();
+            return user;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 }
